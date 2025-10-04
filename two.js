01c63c7767e0ad147f1afc871b74e66fa25733fa
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-10-04 16:28:52 +0530

    update

---
+++ two.js
@@ -0,0 +1,28 @@
+/*let fun= ()=>{return "tanmay"}
+console.log(fun())
+
+let demo=(num)=>(num+num)
+let n=demo(8)
+console.log(n)
+
+let a=parseInt(prompt("enter a no"))
+let fun1=(num1)=>{return (num1*num1)}
+console.log(fun1(a))
+
+let l=parseInt(prompt("enter a length"))
+let b=parseInt(prompt("enter a width"))
+let fun2=(num1,num2)=>{return num1*num2}
+console.log(fun2(l,b))
+
+let b=parseInt(prompt("enter base"))
+let h=parseInt(prompt("enter height"))
+let fun3=(num1,num2)=>{return 0.5*num1*num2}
+console.log(fun3(b,h))
+
+let num=parseInt(prompt("enter a no"))
+let fun4=(num1)=>{return num1*num1*num1}
+console.log(fun4(num))*/
+
+let r=parseInt(prompt("enter a radius"))
+let fun5=(num1)=>{return 3.14*num1*num1}
+console.log(fun5(r))
