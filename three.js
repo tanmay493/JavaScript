a0cc81c76de1b49ecf37165fd7ed7624959cde18
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-10-06 17:08:25 +0530

    update

---
+++ three.js
@@ -0,0 +1,20 @@
+document.getElementById('h1').textContent="from js"
+document.getElementById('h2').textContent="from js"
+document.getElementById('h3').textContent="from js"
+document.getElementById('h4').textContent="from js"
+document.getElementById('h5').textContent="from js"
+document.getElementById('h6').textContent="from js"
+
+let a=document.getElementById('lily')
+a.textContent = "TANMAY"
+a.textContent="TANfsdfdsMAY2"
+
+let b=document.getElementById('l1')
+    b.innerHTML="<i> name </i>"
+    b.innerHTML="<u> name 2 </u>"
+    b.setAttribute=('align','center')
+
+ let c=document.body.setAttribute('bgcolor','yellow')
+  document.getElementById('l2').removeAttribute('style')
+ 
+    
