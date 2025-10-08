2ea966f1fcafdb9cb29f9ecaf81ded2a79966bfd
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-10-08 07:06:40 +0530

    update

---
+++ four.js
@@ -0,0 +1,15 @@
+let a=document.getElementById('2')
+a.style.cssText="background-color:green;color:white;padding:12px"
+
+let a1=document.getElementById('4')
+a1.style.cssText="background-color:green;color:white;padding:12px"
+
+let a2=document.getElementById('6')
+a2.style.cssText="background-color:green;color:white;padding:12px"
+
+let a3=document.getElementById('8')
+a3.style.cssText="background-color:green;color:white;padding:12px"
+
+let a4=document.getElementById('10')
+a4.style.cssText="background-color:green;color:white;padding:12px"
+
--- three.js
+++ three.js
@@ -7,14 +7,38 @@ document.getElementById('h6').textContent="from js"
 
 let a=document.getElementById('lily')
 a.textContent = "TANMAY"
-a.textContent="TANfsdfdsMAY2"
+a.setAttribute('align','center')
+
 
 let b=document.getElementById('l1')
     b.innerHTML="<i> name </i>"
-    b.innerHTML="<u> name 2 </u>"
-    b.setAttribute=('align','center')
+    b.innerHTML=" name 2 "
+    b.setAttribute('align','right')
 
  let c=document.body.setAttribute('bgcolor','yellow')
-  document.getElementById('l2').removeAttribute('style')
+  
+ document.getElementById('l2').removeAttribute('style')
+
+ document.getElementById('rose').setAttribute('src','moon2.jpeg')
+
+ //for using css in js 'style' keyword necessary
+ document.getElementById('lotus').style.backgroundColor='white'
+
+ let n1=document.getElementById('sin')
+ n1.style.backgroundColor='lightblue'
+ n1.style.padding='20px'
+
+ let q=document.getElementById('cos').style.backgroundColor='lightgreen'
+ document.getElementById('tan').style.backgroundColor='lightpink'
+ document.getElementById('sec').style.backgroundColor='aliceblue'
+ 
+ let t=document.getElementById('cosec')
+ t.style.cssText="background-color:black;color:white;padding:10px"
+
+ let w=document.createElement('p')
+ w.textContent="My name is tanmay"
+ //console.log(w) //for checking in console
+
+ document.getElementById('abc').append(w)
  
     
