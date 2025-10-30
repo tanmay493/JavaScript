4705406af2338bd54d9e9b0d325ed7a03c5339c8
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-10-30 15:27:47 +0530

    update

---
+++ 22.js
@@ -0,0 +1,8 @@
+function fun(){
+    let num1=parseInt(document.querySelector('#num1').value)
+    let num2=parseInt(document.querySelector('#num2').value)
+
+    alert(num1+num2)
+   let x= document.querySelector('#h2')
+   x.textContent=(num1+num2)
+}
---
+++ 24.js
@@ -0,0 +1,16 @@
+function fun(){
+    let name=document.querySelector('#name').value
+    let age=document.querySelector('#age').value
+    let city=document.querySelector('#city').value
+    let contact=document.querySelector('#contact').value
+    let branch=document.querySelector('#branch').value
+
+    let x=document.querySelector('#x').innerHTML=name
+    let y=document.querySelector('#y').innerHTML=age
+
+    let z=document.querySelector('#z').innerHTML=city
+    let a=document.querySelector('#a').innerHTML=contact
+    let b=document.querySelector('#b').innerHTML=branch
+    
+return false
+}
