811068e9fc9648a7a8d6ce316fb7a3f4c6a2d891
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-11-03 17:13:34 +0530

    update

---
+++ 29.js
@@ -0,0 +1,16 @@
+/* Note: remove wale array functions like pop() and shift()
+me argument nahi pass karte*/
+let ar=["java","php","python"]
+console.log(ar)
+
+ar.push("node.js","react.js")
+console.log(ar)
+
+ar.pop()
+console.log(ar)
+
+ar.unshift("C","Assembly")
+console.log(ar)
+
+ar.shift()
+console.log(ar)
