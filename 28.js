11d697c3f450e298e3e13574a08af9c1b55ca365
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-11-03 16:15:20 +0530

    updates

---
+++ 28.js
@@ -0,0 +1,15 @@
+/*let ar=["tanmay",26,"bhopal","india"]
+for(let vl of ar){
+    console.log(vl)
+}*/
+
+let ar=[25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+for(let i of ar){
+    if(i%2==0){
+        console.log(i,i*i)
+    }
+}
+
+for(let j in ar){
+    console.log(j)
+}
