d9ffd14a99a3bf370a06cb1d56578dcc1cc7b2b2
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-11-14 09:34:26 +0530

    string and functions

--- b1.js
+++ b1.js
@@ -1,7 +1,42 @@
 function fun(){
-    let name=document.querySelector('#name').value()
-    let city=document.querySelector('#city').value()
-    let state=document.querySelector('#state').value()
+    let name=document.querySelector('#name').value
+    let city=document.querySelector('#city').value
+    let state=document.querySelector('#state').value
+    let uname=name.toUpperCase()
+    let ucity=city.toUpperCase()
+    let ustate=city.toUpperCase()
+
+    if(name==""){
+        alert("please fill ur name")
+        
+        document.querySelector('#name').focus()
+        
+    
+        return false
+    }
+  else  if(city==""){
+        alert("please fill ur city")
+        
+        document.querySelector('#city').focus()
+        
+        
+        return false
+    }
+    else if(state==""){
+        alert("please fill ur state")
+        
+        document.querySelector('#state').focus()
+    
+        
+        return false
+    }
+
+    let a=document.querySelector('#a').innerHTML=uname
+    let b=document.querySelector('#b').innerHTML=city
+    let c=document.querySelector('#c').innerHTML=state
+    
+ return false
+
 
     
 }
